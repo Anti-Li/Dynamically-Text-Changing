ebee049c88d2950d137ab830e01a4bdd78ce927a
Andy Li <andylizf@outlook.com>
2018-01-31 15:24:57 +0800

    Change gulpfile.js. Reduce HTML and CSS.
    Reduce redundant HTML tag[div], so corresponding reduce the CSS. Let CSS be readable.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
     uncss = require('gulp-uncss');
-gulp.task('uncss', function () {
+gulp.task('default', function () {
     gulp.src("*.css")//css dir
     .pipe(uncss({
         html: ["*.html"]
         //html dir
     }))
-    .pipe(gulp.dest("dist/uncss2"));
+    .pipe(gulp.dest("uncss2"));
     //new css dir
 });
